e45fa173846018ad2469c7d1ed3c5d9b9a5eb3ed
Mohamed Abdifitah Ahmed <learntecnology30@gmail.com>
2023-10-05 17:51:44 +0300

    main file to normal

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -9,10 +8,9 @@ import (
 	"github.com/mohamedabdifitah/ecapi/api"
 	"github.com/mohamedabdifitah/ecapi/db"
 	"github.com/mohamedabdifitah/ecapi/service"
-	"github.com/mohamedabdifitah/ecapi/template"
 )
 
-func hell() {
+func main() {
 	if os.Getenv("APP_ENV") == "development" {
 		err := godotenv.Load(".env.local")
 		if err != nil {
@@ -24,22 +22,4 @@ func hell() {
 	db.ConnectDB()
 	service.InitMelliClient()
 	api.Initserver()
-}
-func main() {
-
-	template.AllTemplates.LoadTemplates("./template/template.json")
-	// fmt.Println(template.AllTemplates)
-	temp, err := template.AllTemplates.TempelateInjector("SwaggGenPath", map[string]string{
-		"Path":        "/custom/swagger",
-		"method":      "get",
-		"tags":        "drivers",
-		"summary":     "h",
-		"description": "h",
-		"operationId": "",
-		"responses":   "",
-	})
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Println(temp)
 }
