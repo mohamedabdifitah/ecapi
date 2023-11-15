899a8e70e01e5c7c6526affaa7a6417404242ca1
Mohamed Abdifitah Ahmed <learntecnology30@gmail.com>
2023-11-15 15:46:54 +0300

    searchable_attributes and retrivable_attributes in search query

--- controller/search.go
+++ controller/search.go
@@ -6,10 +6,28 @@ import (
 	"github.com/mohamedabdifitah/ecapi/service"
 )
 
+var menu_searchable_attributes []string = []string{"title", "description", "category", "reciepe", "price"}
+var merchant_searchable_attributes []string = []string{"business_name", "generes", "description", "address"}
+var merchant_retivable_attributes []string = []string{"id", "business_name", "generes", "description", "location", "address", "closed", "likes"}
+var menu_retivable_attributes []string = []string{"id", "title", "description", "category", "reciepe", "merchant_external_id", "price", "service_availablity"}
+
 func Search(c *gin.Context) {
 	var query string = c.Query("query")
 	var index string = c.Query("index")
-	var body meilisearch.SearchRequest
+	var searchable_attributes []string
+	var retivable_attributes []string
+	switch index {
+	case "merchant":
+		searchable_attributes = merchant_searchable_attributes
+		retivable_attributes = merchant_retivable_attributes
+	case "menu":
+		searchable_attributes = menu_searchable_attributes
+		retivable_attributes = menu_retivable_attributes
+	}
+	var body meilisearch.SearchRequest = meilisearch.SearchRequest{
+		AttributesToSearchOn: searchable_attributes,
+		AttributesToRetrieve: retivable_attributes,
+	}
 	err := c.ShouldBindJSON(&body)
 	if err != nil {
 		c.JSON(400, err.Error())
@@ -24,29 +42,31 @@ func Search(c *gin.Context) {
 	c.JSON(200, &res)
 }
 func MultiSearch(c *gin.Context) {
-	var queries []meilisearch.SearchRequest
-	err := c.ShouldBindJSON(&queries)
+	var body struct {
+		Menu     string `json:"menu"`
+		Merchant string `json:"merchant"`
+	}
+	err := c.ShouldBindJSON(&body)
 	if err != nil {
 		c.JSON(200, err)
 		return
 	}
-	// query := []meilisearch.SearchRequest{
-	// 	{
-	// 		IndexUID: "movies",
-	// 		Query:    "pooh",
-	// 		Limit:    5,
-	// 	},
-	// 	{
-	// 		IndexUID: "movies",
-	// 		Query:    "nemo",
-	// 		Limit:    5,
-	// 	},
-	// 	{
-	// 		IndexUID: "movie_ratings",
-	// 		Query:    "us",
-	// 	},
-	// }
-	res, err := service.MultiSearch(queries)
+	query := []meilisearch.SearchRequest{
+		{
+			IndexUID:             "menu",
+			Query:                body.Menu,
+			AttributesToRetrieve: menu_retivable_attributes,
+			AttributesToSearchOn: menu_searchable_attributes,
+		},
+		{
+
+			IndexUID:             "merchant",
+			Query:                body.Merchant,
+			AttributesToRetrieve: merchant_retivable_attributes,
+			AttributesToSearchOn: merchant_searchable_attributes,
+		},
+	}
+	res, err := service.MultiSearch(query)
 	if err != nil {
 		c.String(400, err.Error())
 		return
